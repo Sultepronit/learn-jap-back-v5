0c9da4074f569c13e17a83f22292fdca60c53067
Sultepronit <sultepronit@gmail.com>
2024-02-18 16:45:33 +0200

    added jooyoo to the db

--- prep/index.php
+++ prep/index.php
@@ -1,10 +1,10 @@
 <?php
-$dbImport = file_get_contents('jap_words (3).sql');
+$dbImport = file_get_contents('jooyoo.sql');
 $pdo = new PDO('sqlite:' . __DIR__ . '/../jap.sqlite');
 print_r($pdo);
 
-// $pdo->exec('drop table jap_words');
-// $pdo->exec($dbImport);
+$pdo->exec('drop table jooyoo');
+$pdo->exec($dbImport);
 
 // $query = "UPDATE jap_words_consts_vars
 //     SET value = 9261
@@ -13,7 +13,9 @@ print_r($pdo);
 
 // $thedb = $pdo->query('SELECT * FROM collected_kanji')->fetchAll(PDO::FETCH_ASSOC);
 // $thedb = $pdo->query('SELECT * FROM collected_kanji_consts_vars')->fetchAll(PDO::FETCH_ASSOC);
-$thedb = $pdo->query('SELECT * FROM jap_words_consts_vars')->fetchAll(PDO::FETCH_KEY_PAIR);
+// $thedb = $pdo->query('SELECT * FROM jap_words_consts_vars')->fetchAll(PDO::FETCH_KEY_PAIR);
+$thedb = $pdo->query('SELECT * FROM jooyoo')->fetchAll(PDO::FETCH_ASSOC);
+
 echo '<pre>';
 print_r($thedb);
 
