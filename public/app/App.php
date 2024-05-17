56039004ea9e05d1d584b11b1955f18e551d62f4
Sultepronit <sultepronit@gmail.com>
2024-05-17 15:29:40 +0300

    refactored; continue developing

--- public/app/App.php
+++ public/app/App.php
@@ -1,55 +1,27 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/controllers/TableController.php';
+require_once __DIR__ . '/Router.php';
 
 class App
 {
-    private PDO $pdo;
-
-    public function __construct()
-    {
-        $this->pdo = new PDO('sqlite:' . __DIR__ . '/../db/db.sqlite');
-    }
-    private function parseRequest(): array
-    {
-        // echo '<pre>';
-        // print_r($_SERVER);
-
-        $ruri = $_SERVER['REQUEST_URI'] ?? '';
-        $rarray = explode('/', $ruri);
-
-        $subject = $rarray[2];
-        $details = array_slice($rarray, 3);
-
-        return [
-            'subject' => $subject,
-            'details' => $details
-        ];
-    }
-
     public function run()
     {
-        $request = $this->parseRequest();
+        try {
+            $pdo = new PDO('sqlite:' . __DIR__ . '/../db/db.sqlite');
 
-        // echo '<pre>';
-        // print_r($request);
-        // echo '</pre>';
+            $response = Router::run($pdo); 
 
-        switch ($request['subject']) {
-            case 'table':
-                // new TableController($request['details'], $this->pdo);
-                TableController::handle($request['details'], $this->pdo);
-                break;
-            case 'session':
-                echo 'Let\'s start a session!';
-                break;
-            default:
-                http_response_code(404);
-                echo '<h2>You shall not pass!</h2><h1>🧙🏻‍♂️</h1>';
+            if($response) {
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            }
+        } catch (\Throwable $th) {
+            http_response_code(500);
+            // echo '<pre>';
+            print_r($th);
+        } finally {
+            $pdo = null;
         }
-
-        // call_user_method();
-        // call_user_method_array();
     }
 }
---
+++ public/app/Router.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/controllers/TableController.php';
+
+class Router
+{
+    private static function parseRequest(): array
+    {
+        // echo '<pre>';
+        // print_r($_SERVER);
+
+        $ruri = $_SERVER['REQUEST_URI'] ?? '';
+        $rarray = explode('/', $ruri);
+
+        $details = array_slice($rarray, 3);
+
+        return [
+            'subject' => $rarray[2],
+            'details' => $details
+        ];
+    }
+
+    public static function run(PDO $pdo): ?array
+    {
+        $request = self::parseRequest();
+
+        // echo '<pre>';
+        // print_r($request);
+        // echo '</pre>';
+
+        switch ($request['subject']) {
+            case 'table':
+                return TableController::handle($request['details'], $pdo);
+            case 'session':
+                echo 'Let\'s start a session!';
+                break;
+            default:
+                http_response_code(404);
+                echo '<h2>You shall not pass!</h2><h1>🧙🏻‍♂️</h1>';
+                return null;
+        }
+    }
+}
