02f7598e1ebd2b47155fed50b99c79e320dd46fb
Sultepronit <sultepronit@gmail.com>
2024-05-25 09:42:40 +0300

    created words-ready session

--- public/app/session.php
+++ public/app/session.php
@@ -8,6 +8,7 @@ require_once 'sessions/wordsForKanji.php';
 require_once 'sessions/kanjiForWords.php';
 require_once 'sessions/KanjiCollector.php';
 require_once 'sessions/wordsMin.php';
+require_once 'sessions/WordsReady.php';
 
 function session(string $sessionName, PDO $pdo)
 {    
@@ -29,6 +30,9 @@ function session(string $sessionName, PDO $pdo)
 
         case 'words-min':
             return wordsMin($pdo);
+        
+        case 'words-ready':
+            return WordsReady::prepare($pdo);
             
         default:
             return ['404' => 'Nothing here!'];
---
+++ public/app/sessions/WordsReady.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+require_once 'words.php';
+
+class WordsReady
+{
+    private static $data = [];
+
+    private static $confirmDivisor = 4;
+    private static $recognizeDivisor = 5;
+
+    private static $learnStages = [
+        'LEARN' => 'learn',
+        'CONFIRM' => 'confirm',
+        'REPEAT' => 'repeat',
+        'AUTOREPEAT' => 'autorepeat',
+        'RECOGNIZE' => 'recognize',
+    ];
+
+    private static function assignLearnStage(array &$list, string $stageLabel): void
+    {
+        foreach($list as &$card) {
+            $card['learnStage'] = self::$learnStages[$stageLabel];
+        }
+    }
+
+    private static function prepareLearnList(): array
+    {
+        $learnList = self::$data['learnList'];
+        self::assignLearnStage($learnList, 'LEARN');
+        return $learnList;
+    }
+
+    private static function getPartOfList(string $listName, int $length, string $stageLabel): array
+    {
+        shuffle(self::$data[$listName]);
+        $result = array_slice(self::$data[$listName], 0, $length);
+        self::assignLearnStage($result, $stageLabel);
+
+        return $result;
+    }
+
+    private static function prepareRepeatList(int $repeatNumber): array
+    {
+        shuffle(self::$data['repeatList']);
+        $repeatList = [];
+
+        $normal = 0;
+        foreach(self::$data['repeatList'] as $card) {
+            if(
+                $card['fProgress'] === 0 && $card['fAutorepeat']
+                || $card['fProgress'] > 0 && $card['bAutorepeat']
+            ) {
+                $card['learnStage'] = self::$learnStages['AUTOREPEAT'];
+            } else {
+                $card['learnStage'] = self::$learnStages['REPEAT'];
+                $normal++;
+            }
+
+            $repeatList[] = $card;
+            if($normal >= $repeatNumber) break;
+        }
+
+        return $repeatList;
+    }
+
+    public static function prepare(PDO $pdo): array
+    {
+        self::$data = words($pdo);
+
+        $sessionLength = self::$data['constsAndVars']['sessionLength'];
+
+        $learnList = self::prepareLearnList();
+        $learnNumber = count($learnList);
+
+        $confirmNumber = (int) (count(self::$data['confirmList']) / self::$confirmDivisor);
+        $confirmList = self::getPartOfList('confirmList', $confirmNumber, 'CONFIRM');
+
+        $recognizeNumber = (int) ceil(count(self::$data['recognizeList']) / self::$recognizeDivisor);
+        $recognizeList = self::getPartOfList('recognizeList', $recognizeNumber, 'RECOGNIZE');
+
+        $contents = [...$learnList, ...$confirmList, ...$recognizeList];
+
+        $repeatNumber = $sessionLength - count($contents);
+        $repeatList = self::prepareRepeatList($repeatNumber);
+
+        $contents = [...$contents, ...$repeatList];
+        shuffle($contents);
+
+        $plan = compact('sessionLength', 'learnNumber', 'confirmNumber', 'recognizeNumber', 'repeatNumber');
+        
+        return compact('plan', 'contents');
+    }
+}
