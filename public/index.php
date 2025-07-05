f4a3fa7541303e9ff053a6ac0b23f1389e58f0af
sultepronit <sultepronit@gmail.com>
2025-07-05 17:50:53 +0000

    updated the db

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once './app/run.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
+header('Access-Control-Max-Age: 86400');
 
 if(!isset($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
