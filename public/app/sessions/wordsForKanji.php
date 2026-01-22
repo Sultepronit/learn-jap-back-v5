dc54bfac2e7342d81a4f08ff8dc6412c36b4f76b
sultepronit <sultepronit@gmail.com>
2026-01-22 14:35:56 +0000

    fixed a bug

--- public/app/sessions/wordsForKanji.php
+++ public/app/sessions/wordsForKanji.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types=1);
 
+// WHERE repeat_status >= 0";
 function wordsForKanji(PDO $pdo) {
     $query = "SELECT alt_writing, writings, rare_writings, readings, rare_readings, translation
     FROM words
-    WHERE repeat_status >= 0";
+    WHERE repeat_status != -1";
     $stmt = $pdo->query($query);
     $words = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
