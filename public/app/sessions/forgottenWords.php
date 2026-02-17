f906793ac71708ac831c5ad132dfcc636208edc1
sultepronit <sultepronit@gmail.com>
2026-02-17 19:59:00 +0000

    now we are returning more than one forgotten word

--- public/app/sessions/forgottenWords.php
+++ public/app/sessions/forgottenWords.php
@@ -2,22 +2,24 @@
 declare(strict_types=1);
 
 function returnForgottenWord(PDO $pdo) {
-    $query = 'SELECT * FROM words WHERE repeat_status = -2;';
+    $query = 'SELECT id FROM words WHERE repeat_status = -2;';
     $stmt = $pdo->query($query);
-    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $list = $stmt->fetchAll(PDO::FETCH_COLUMN);
     // print_r(json_encode($list));
-
     $len = count($list);
-    // print_r($list[$len - 1]);
-    $index = random_int(0, $len + 10);
-    // echo $index . PHP_EOL;
-    if ($index >= $len) return;
+    
+    for ($i = 0; $i < $len / 20; $i++) {
+        $index = random_int(0, $len + 10);
+        echo $index . PHP_EOL;
+        if ($index >= $len) continue;
 
-    $id = $list[$index]['id'];
-    // echo $id . PHP_EOL;
-    $query = "UPDATE words SET repeat_status = 0 WHERE id = ?";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$id]);
-    // echo json_encode($list[$index]);
+        $id = $list[$index];
+        echo $id . PHP_EOL;
+        
+        $query = "UPDATE words SET repeat_status = 0 WHERE id = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$id]);
+    }
+    
     // exit;
 }
